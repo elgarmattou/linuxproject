db4ec9aeac02210bb77a7189a72006fbfa98db61
elgarmattou <elgarmattou@gmail.com>
2024-03-22 09:59:23 +0000

    firebase link

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD0BtGOqd7JxAT2Dbc2gnXV0XQSR2oDqnU",
+    authDomain: "test-linux-d2dc4.firebaseapp.com",
+    projectId: "test-linux-d2dc4",
+    storageBucket: "test-linux-d2dc4.appspot.com",
+    messagingSenderId: "831505529160",
+    appId: "1:831505529160:web:c392066b6fc4af701b1b7d",
+    measurementId: "G-MH3CJ1C4R9"
+  };
